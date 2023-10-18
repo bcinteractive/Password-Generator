c6415d7b99e1307a0a1364109cd660a3ed3d2800
bcinteractive <bcinteractive@users.noreply.github.com>
2023-10-18 22:10:17 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let generateBtn = document.getElementById("generate-btn")
+let password1El = document.getElementById("password1-el")
+let password2El = document.getElementById("password2-el")
+// let passwordLength = document.getElementById("password-length-num")
+let clipBoard = navigator.clipboard
+
+generateBtn.addEventListener("click", function () {
+    password1El.textContent = getPassword()
+    password2El.textContent = getPassword()
+})
+
+password1El.addEventListener("click", async function() {
+   try {
+        await copy(password1El.textContent);
+    } catch(e) {
+        console.error(e);
+    }
+})
+
+password2El.addEventListener("click", async function() {
+    try {
+        await copy(password2El.textContent);
+    } catch(e) {
+        console.error(e);
+    }
+})
+
+function getPassword() {
+    let password = ""
+    for (let i = 0; i < 15; i++) {
+        password += characters[Math.floor(Math.random()*characters.length)]
+    }
+    return password
+}
+
+function copy(text) {
+    return new Promise((resolve, reject) => {
+        if (typeof navigator !== "undefined" && typeof navigator.clipboard !== "undefined" && navigator.permissions !== "undefined") {
+            const type = "text/plain";
+            const blob = new Blob([text], { type });
+            const data = [new ClipboardItem({ [type]: blob })];
+            navigator.permissions.query({name: "clipboard-write"}).then((permission) => {
+                if (permission.state === "granted" || permission.state === "prompt") {
+                    navigator.clipboard.write(data).then(resolve, reject).catch(reject);
+                }
+                else {
+                    reject(new Error("Permission not granted!"));
+                }
+            });
+        }
+        else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+            var textarea = document.createElement("textarea");
+            textarea.textContent = text;
+            textarea.style.position = "fixed";
+            textarea.style.width = '2em';
+            textarea.style.height = '2em';
+            textarea.style.padding = 0;
+            textarea.style.border = 'none';
+            textarea.style.outline = 'none';
+            textarea.style.boxShadow = 'none';
+            textarea.style.background = 'transparent';
+            document.body.appendChild(textarea);
+            textarea.focus();
+            textarea.select();
+            try {
+                document.execCommand("copy");
+                document.body.removeChild(textarea);
+                resolve();
+            }
+            catch (e) {
+                document.body.removeChild(textarea);
+                reject(e);
+            }
+        }
+        else {
+            reject(new Error("None of copying methods are supported by this browser!"));
+        }
+    });
+    
+}
+
